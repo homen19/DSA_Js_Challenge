963029128fef6af5527afd33c7e78bbae5c9fb5b
Homen Nath <homennath7638@gmail.com>
2023-10-28 10:18:57 +0530

    Object Destructuring done

---
+++ Objects/Obj3.js
@@ -0,0 +1,15 @@
+
+const course = {
+    courseName : 'Js in Hindi',
+    price : "999",
+    courseTeacher : "hitesh"
+}
+
+
+// console.log(course["courseTeacher"])
+
+const { courseTeacher, price, courseName } = course;
+
+console.log(courseTeacher);
+console.log(courseName);
+console.log(price);
